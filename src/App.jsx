7f7c32acae0d39a9a9311e99467c13e23e0751f5
Tom Hestamp <tom@hestamp.me>
2022-12-03 03:04:47 +0200

    so push

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import React from 'react'
 import { useEffect } from 'react'
 
 import './App.css'
+
 import Header from './components/Header/Header'
+import { useTelegram } from './components/hooks/useTelegram'
 
 function App() {
-  const tg = window.Telegram.WebApp
+  const { tg, onToggleButton } = useTelegram()
+
   useEffect(() => {
     tg.ready()
   }, [])
 
   return (
     <div className="App">
-      <Header />
+      {/* <Header /> */}
+      <button onClick={onToggleButton}>Toggle</button>
     </div>
   )
 }
