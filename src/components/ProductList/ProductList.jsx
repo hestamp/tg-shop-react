2ac0830321556e3877d9f90c706fd5e28118a05a
Tom Hestamp <tom@hestamp.me>
2022-12-04 01:54:20 +0200

    asd

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -77,7 +77,7 @@ const getTotalPrice = (items) => {
 }
 
 const ProductList = () => {
-  const [shopList, setShopList] = useState([3])
+  const [shopList, setShopList] = useState([])
   const { tg, queryId } = useTelegram()
 
   const onSendData = useCallback(() => {
