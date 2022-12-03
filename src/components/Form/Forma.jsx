251cdf7b73982af548539bc336887bdfcb180b51
Tom Hestamp <tom@hestamp.me>
2022-12-03 21:13:42 +0200

    asa

--- src/components/Form/Forma.jsx
+++ src/components/Form/Forma.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { useEffect } from 'react'
 import { useTelegram } from '../../hooks/useTelegram'
 import './Forma.css'
